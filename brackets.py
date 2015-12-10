3655a78b84f9a407963e3cd60efae2cf6c00e1e1
Abiodun Shuaib <abiodun.shauib.m@gmail.com>
2015-12-10 14:39:15 +0100

    brackets code from chidi

---
+++ brackets.py
@@ -0,0 +1,18 @@
+# Implement an algorithm to print all valid (e.g., properly opened and
+# closed) combinations of n-pairs of parentheses.
+
+# ```EXAMPLE:
+# Input: 3 (e.g., 3 pairs of parentheses)
+# Output: ()()(), ()(()), (())(), ((()))
+
+
+def printbrackets(p, i):
+    if i < int(n):
+        printbrackets(p+'()', i+1)
+        printbrackets('('+p+')', i+1)
+    else:
+        print p
+
+
+n = input("type the number of brackets you want to permutate: ")
+printbrackets('()', 1)
